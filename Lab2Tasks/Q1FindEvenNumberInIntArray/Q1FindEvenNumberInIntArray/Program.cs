cc3ca680e13b0da43944ded69a2bc2d1d6e08139
FaizanButt1997 <fazybutt1997@gmail.com>
2018-07-24 14:08:36 -0700

    Initial commit

---
+++ Lab2Tasks/Q1FindEvenNumberInIntArray/Q1FindEvenNumberInIntArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1FindEvenNumberInIntArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            Program p = new Program();
+
+           int total = p.EvenNumber_arr(arr);
+            Console.WriteLine(" Sum of Even Number " + total );
+            Console.ReadKey();
+        }
+
+        public int EvenNumber_arr(int[] arr)
+        {
+            //1st way
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+
+                    sum += arr[i];
+                }
+            }
+            return sum;
+
+            //2nd way
+
+            //return arr.Where(x => x % 2 == 0).Sum();
+
+        }
+
+    }
+}
---
+++ Lab2Tasks/Q2Sorting/Q2Sorting/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, 4, 2, 3, 1, 5 };
+
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
